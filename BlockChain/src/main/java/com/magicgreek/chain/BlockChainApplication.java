58d2c635aee1bb63fef319598f7888d49c66ab69
magicgreek <magicgreek@163.com>
2018-11-19 14:35:58 +0800

    init

---
+++ BlockChain/src/main/java/com/magicgreek/chain/BlockChainApplication.java
@@ -0,0 +1,12 @@
+package com.magicgreek.chain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockChainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockChainApplication.class, args);
+	}
+}
