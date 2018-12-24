abf26607a8167ced80f118e979fad28d4de1f785
magicgreek <magicgreek@163.com>
2018-12-24 20:20:54 +0800

    bug修改

--- BlockChain/src/main/java/com/chain/task/dao/JobAndTriggerMapper.java
+++ BlockChain/src/main/java/com/chain/task/dao/JobAndTriggerMapper.java
@@ -2,11 +2,14 @@ package com.chain.task.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.chain.task.entity.JobAndTrigger;
 
 /**
  * @ClassName: JobAndTriggerMapper
  */
+@Mapper
 public interface JobAndTriggerMapper {
     List<JobAndTrigger> getJobAndTriggerDetails();
 
